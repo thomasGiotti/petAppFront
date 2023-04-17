22cf7ce5e2a9e37daa00e008d1102095b753b4e2
thomas.giotti <tho.giotti@gmail.com>
2023-04-17 21:54:02 +0200

    feat(start): start project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    Plugin: [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          src: './src'
+        }
+      }
+    ]
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+function App() {
+  return <SafeAreaProvider>...</SafeAreaProvider>
+}
+
+export default App
